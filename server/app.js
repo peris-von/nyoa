0c0d85ed9d39d8c27ee24d77f35e732496a7d4a7
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2026-07-17 16:33:25 +0300

    we

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const paymentRoutes = require("./routes/payment.routes");
 const cors = require("cors");
 const app = express();
-
+//
 app.use(cors());
 
 app.use(express.json());
